44eb5572a4e45337fff388fd41f97ea708a3e5ef
SirArkimedes <sirarkimedes@gmail.com>
2017-10-19 08:20:00 -0500

    Add basic introduction of program

--- Project6/Project6/Program.cs
+++ Project6/Project6/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿/******************************************************/
+/* Andrew Robinson                                    */
+/* Math 371                                           */
+/* Fall 2017                                          */
+/*                                                    */
+/* Source at: https://github.com/SirArkimedes/MATH371 */
+/******************************************************/
 
+// Imports
+using System;
+
+// Declare the project's namespace...
 namespace Project6
 {
-    class MainClass
+
+	// Declare a struct for easy reuse later in the course...
+	struct ProgramDescriptions
+	{
+		// Accepts a string that is displayed with the standard information.
+		public static void displayClassInformation(string programName)
+		{
+			Console.WriteLine("Math 371");
+			Console.WriteLine("Fall 2017");
+			Console.WriteLine(programName);
+			Console.WriteLine("Andrew Robinson");
+			Console.WriteLine();
+		}
+
+		// Accepts the purpose string to display in informative label.
+		public static void displayPurpose(string purpose)
+		{
+			Console.WriteLine("Purpose:");
+			Console.WriteLine(purpose);
+			Console.WriteLine();
+		}
+	}
+
+	// Declare the class that Main() enters into.
+	class MainClass
     {
-        public static void Main(string[] args)
+		// Main() -> The console enters into here...
+		public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			// Call the struct declared above and pass the necessary information.
+			ProgramDescriptions.displayClassInformation("Implement shortest patch algorithm");
+
+			string purpose = "Upon input of a, b, c, d, r, and s, the solution to " +
+				" the system of equations ax + by = r, cx + dy = s is produced.";
+			ProgramDescriptions.displayPurpose(purpose);
         }
     }
 }
