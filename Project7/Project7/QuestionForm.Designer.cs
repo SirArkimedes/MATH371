6b6d2d48cd7db8f8f472b41aa8e8d6714d626b50
Andrew Robinson <sirarkimedes@gmail.com>
2017-11-17 16:08:02 -0600

    Start Project 7

---
+++ Project7/Project7/QuestionForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Project7
+{
+  partial class QuestionForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.firstButton = new System.Windows.Forms.Button();
+      this.secondButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // firstButton
+      // 
+      this.firstButton.Location = new System.Drawing.Point(79, 39);
+      this.firstButton.Name = "firstButton";
+      this.firstButton.Size = new System.Drawing.Size(153, 53);
+      this.firstButton.TabIndex = 0;
+      this.firstButton.Text = "Barge problem";
+      this.firstButton.UseVisualStyleBackColor = true;
+      this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+      // 
+      // secondButton
+      // 
+      this.secondButton.Location = new System.Drawing.Point(79, 143);
+      this.secondButton.Name = "secondButton";
+      this.secondButton.Size = new System.Drawing.Size(153, 53);
+      this.secondButton.TabIndex = 1;
+      this.secondButton.Text = "The other one";
+      this.secondButton.UseVisualStyleBackColor = true;
+      this.secondButton.Click += new System.EventHandler(this.secondButton_Click);
+      // 
+      // QuestionForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(318, 243);
+      this.Controls.Add(this.secondButton);
+      this.Controls.Add(this.firstButton);
+      this.Name = "QuestionForm";
+      this.Text = "Which do you wish to solve?";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button firstButton;
+    private System.Windows.Forms.Button secondButton;
+  }
+}
+
---
+++ Project7/Project7/QuestionForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project7
+{
+  public partial class QuestionForm : Form
+  {
+    public QuestionForm()
+    {
+      InitializeComponent();
+    }
+
+    private void firstButton_Click(object sender, EventArgs e)
+    {
+      // Open other form.
+    }
+
+    private void secondButton_Click(object sender, EventArgs e)
+    {
+      // Open other form.
+    }
+
+  }
+}
