8db5a91fe5a93235fe5f59d2e06e807739ec68e1
Andrew Robinson <sirarkimedes@gmail.com>
2017-11-17 22:55:06 -0600

    Fancy alphabet calculation for item name

--- Project7/Project7/Bardge.Designer.cs
+++ Project7/Project7/Bardge.Designer.cs
@@ -28,14 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             this.inputDataGrid = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.companyNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.weightColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.costColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.inputDataGrid)).BeginInit();
@@ -45,13 +45,42 @@
             // 
             this.inputDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.inputDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.companyNameColumn,
+            this.nameColumn,
             this.weightColumn,
             this.costColumn});
             this.inputDataGrid.Location = new System.Drawing.Point(12, 80);
             this.inputDataGrid.Name = "inputDataGrid";
             this.inputDataGrid.Size = new System.Drawing.Size(236, 366);
             this.inputDataGrid.TabIndex = 0;
+            this.inputDataGrid.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.inputDataGrid_DefaultValuesNeeded);
+            // 
+            // nameColumn
+            // 
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.nameColumn.DefaultCellStyle = dataGridViewCellStyle7;
+            this.nameColumn.HeaderText = "Name";
+            this.nameColumn.MaxInputLength = 10;
+            this.nameColumn.Name = "nameColumn";
+            this.nameColumn.ReadOnly = true;
+            this.nameColumn.Width = 55;
+            // 
+            // weightColumn
+            // 
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.weightColumn.DefaultCellStyle = dataGridViewCellStyle8;
+            this.weightColumn.HeaderText = "Weight";
+            this.weightColumn.MaxInputLength = 10;
+            this.weightColumn.Name = "weightColumn";
+            this.weightColumn.Width = 60;
+            // 
+            // costColumn
+            // 
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.costColumn.DefaultCellStyle = dataGridViewCellStyle9;
+            this.costColumn.HeaderText = "Cost";
+            this.costColumn.MaxInputLength = 10;
+            this.costColumn.Name = "costColumn";
+            this.costColumn.Width = 60;
             // 
             // label1
             // 
@@ -61,34 +90,6 @@
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Input data";
-            // 
-            // companyNameColumn
-            // 
-            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.companyNameColumn.DefaultCellStyle = dataGridViewCellStyle19;
-            this.companyNameColumn.HeaderText = "Company name";
-            this.companyNameColumn.MaxInputLength = 10;
-            this.companyNameColumn.Name = "companyNameColumn";
-            this.companyNameColumn.ReadOnly = true;
-            this.companyNameColumn.Width = 55;
-            // 
-            // weightColumn
-            // 
-            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.weightColumn.DefaultCellStyle = dataGridViewCellStyle20;
-            this.weightColumn.HeaderText = "Weight";
-            this.weightColumn.MaxInputLength = 10;
-            this.weightColumn.Name = "weightColumn";
-            this.weightColumn.Width = 60;
-            // 
-            // costColumn
-            // 
-            dataGridViewCellStyle21.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            this.costColumn.DefaultCellStyle = dataGridViewCellStyle21;
-            this.costColumn.HeaderText = "Cost";
-            this.costColumn.MaxInputLength = 10;
-            this.costColumn.Name = "costColumn";
-            this.costColumn.Width = 60;
             // 
             // textBox1
             // 
@@ -132,10 +133,10 @@
 
         private System.Windows.Forms.DataGridView inputDataGrid;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn companyNameColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn weightColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn costColumn;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weightColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn costColumn;
     }
 }
--- Project7/Project7/Bardge.cs
+++ Project7/Project7/Bardge.cs
@@ -12,15 +12,48 @@ namespace Project7
 {
     public partial class Bardge : Form
     {
+
+        List<string> alphabet = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                                "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+                                "W", "X", "Y", "Z", "ZZZZ" };
+
+        /***********************/
+        /* Init                */
+        /***********************/
+
         public Bardge()
         {
             InitializeComponent();
 
             // Add some default data.
-            inputDataGrid.Rows.Add("1", 1, 10);
-            inputDataGrid.Rows.Add("2", 2, 25);
-            inputDataGrid.Rows.Add("3", 3, 40);
-            inputDataGrid.Rows.Add("4", 4, 60);
+            inputDataGrid.Rows.Add("A", 1, 10);
+            inputDataGrid.Rows.Add("B", 2, 25);
+            inputDataGrid.Rows.Add("C", 3, 40);
+            inputDataGrid.Rows.Add("D", 4, 60);
+        }
+
+        /***********************/
+        /* Event Handlers      */
+        /***********************/
+
+        private void inputDataGrid_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells[0].Value = getAlphabetValue(e.Row.Index);
+        }
+
+        /***********************/
+        /* Helpers             */
+        /***********************/
+
+        private string getAlphabetValue(int index)
+        {
+            string value = "";
+            if (index < alphabet.Count)
+                value = alphabet[index];
+            else
+                value = alphabet[alphabet.Count - 1];
+
+            return value;
         }
     }
 }
