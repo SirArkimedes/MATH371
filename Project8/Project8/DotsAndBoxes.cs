fa6e8f6753940864e563e190b0c53336979721a5
Andrew Robinson <sirarkimedes@gmail.com>
2017-11-20 16:20:13 -0600

    Add basic game

--- Project8/Project8/DotsAndBoxes.cs
+++ Project8/Project8/DotsAndBoxes.cs
@@ -14,6 +14,8 @@ namespace Project8
     {
         const uint sizeOfGrid = 9;
 
+        private Game game;
+
         public DotsAndBoxes()
         {
             InitializeComponent();
@@ -21,6 +23,8 @@ namespace Project8
 
         private void DotsAndBoxes_Load(object sender, EventArgs e)
         {
+            game = new Game(sizeOfGrid, sizeOfGrid); // Init the game's grid size.
+
             // Create the dots and boxes grid, programmatically.
             Panel lastPanel = new Panel();
             for (int row = 0; row < sizeOfGrid; row++)
@@ -35,15 +39,20 @@ namespace Project8
                     button.Text = "";
                     button.Location = new Point(0, 0);
 
+                    // Add these radio buttons to the game.
+                    Dot dot = new Dot(button);
+                    game.dots[row, column] = dot;
+
                     panel.Controls.Add(button);
 
                     Controls.Add(panel);
 
-                    if (column == sizeOfGrid - 1 && row == sizeOfGrid - 1)
+                    if (column == sizeOfGrid - 1 && row == sizeOfGrid - 1) // Is bottom right?
                         lastPanel = panel;
 
                 }
 
+            // Dynamically size the form based on the bottom right Panel that is placed on the form.
             Size = new Size(lastPanel.Location.X + lastPanel.Width + 45 + (45/3),
                             lastPanel.Location.Y + lastPanel.Height + (2 * 45));
         }
---
+++ Project8/Project8/Game.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Project8
+{
+    class Dot
+    {
+        private RadioButton button = new RadioButton();
+
+        public Dot(RadioButton button)
+        {
+            this.button = button;
+        }
+    }
+
+    class Path
+    {
+        
+    }
+
+    class Box
+    {
+
+    }
+
+    // Class that manages the Game's state.
+    class Game
+    {
+        public Dot[,] dots;
+
+        private uint width = 0;
+        private uint height = 0;
+
+        public Game(uint width, uint height)
+        {
+            dots = new Dot[width, height];
+
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
