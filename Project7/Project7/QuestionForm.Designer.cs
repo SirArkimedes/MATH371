fc437b41018c1c71658450872ff1a6c0d8598048
Andrew Robinson <sirarkimedes@gmail.com>
2017-11-19 22:50:52 -0600

    Add informative labels to question form

--- Project7/Project7/QuestionForm.Designer.cs
+++ Project7/Project7/QuestionForm.Designer.cs
@@ -30,6 +30,10 @@
     {
             this.firstButton = new System.Windows.Forms.Button();
             this.secondButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // firstButton
@@ -52,11 +56,51 @@
             this.secondButton.UseVisualStyleBackColor = true;
             this.secondButton.Click += new System.EventHandler(this.secondButton_Click);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Math 371";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(316, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fall 2017";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(316, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Dynamic Programming";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(316, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Andrew Robinson";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // QuestionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 77);
+            this.ClientSize = new System.Drawing.Size(340, 167);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.secondButton);
             this.Controls.Add(this.firstButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -73,6 +117,10 @@
 
     private System.Windows.Forms.Button firstButton;
     private System.Windows.Forms.Button secondButton;
-  }
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
 }
 
